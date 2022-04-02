cfc5d2928e21e5e3f2d3384779aaea2f51ae9e23
Jason Filippou <jason.filippou@gmail.com>
2022-04-02 11:28:05 -0400

    Added DOM challenge solution from section 11.

---
+++ DOM/index.js
@@ -0,0 +1,3 @@
+let allListItems = document.querySelectorAll("li");
+let numberOfListItems = allListItems.length;
+allListItems[numberOfListItems - 1].innerHTML = "Memes";
--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -10,25 +10,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 // The following statement allows our `body-parser` instance to access form-data.
 app.use(bodyParser.urlencoded({extended:true}));
 
-// Set our view engine
+// Set our view engine. This requires that I have a `views` top-level directory in my project.
 app.set('view engine', 'ejs');
+
+const items = ["Buy food", "Cook food", "Eat food"];
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}!`));
 
 app.get("/", (req, res) => {
+    const dateTimeFormatOptions = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    const today = new Date().toLocaleDateString(dateTimeFormatOptions);
+    res.render("list", {day: today, items: items}); // This requires that I have set my view engine appropriately and I have "list" under my views/ subfolder.
+});
 
-    const getDayName = (num) => {
-        switch (num) {
-            case 0: return "Sunday";
-            case 1: return "Monday";
-            case 2: return "Tuesday";
-            case 3: return "Wednesday";
-            case 4: return "Thursday";
-            case 5: return "Friday";
-            case 6: return "Saturday";
-            default: throw "Bad day number provided";
-        }
+app.post("/", (req, res) => {
+    let newItem = req.body.newItem;  // body-parser allows us to fetch this data.
+    if(req.body.newItem) {
+        items.push(newItem);
     }
-
-    res.render("list", {day: getDayName(new Date().getDay())});
+    res.redirect("/");  // Restarting the entire process by triggering a GET at "/".
 });
