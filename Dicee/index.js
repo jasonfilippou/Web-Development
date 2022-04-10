b3327e810b7d5d290c6718742842ad065db4159b
Jason Filippou <jason.filippou@gmail.com>
2022-04-10 15:23:46 -0400

    Finished Dicee challenge.

--- Dicee/index.js
+++ Dicee/index.js
@@ -1,19 +1,38 @@
-var leftDieFace = Math.floor(Math.random() * 6) + 1
-var rightDieFace = Math.floor(Math.random() * 6) + 1
+function randomDieFace(){
+    return "./images/dice" + randomDieFaceValue() + ".png";
+};
 
-document.querySelector("div.dice img.img1").setAttribute("src", "images/dice" + leftDieFace + ".png")
-document.querySelector("div.dice img.img2").setAttribute("src", "images/dice" + rightDieFace + ".png")
-// Let's see if JS can see the "result" variable if I define it inside the conditions
-if(leftDieFace > rightDieFace)
-{
-  result = "Player 1 wins!"
+function randomDieFaceValue(){
+    return randomInt(1, 7); // Bottom inclusive, top exclusive.
+};
+
+function randomInt(low, high){
+  if (!isInt(low) || !isInt(high)){
+    throw "We only accept integer arguments.";
+  }
+  else {
+    return Math.floor(Math.random() * (high - low) + low);
+  }
+
+  function isInt(num) {
+     return $.isNumeric(num) && (Math.floor(num) === num);
+  }
+};
+
+const dieFaceOne = randomDieFace();
+const dieFaceTwo = randomDieFace();
+
+document.querySelector("img.img1").src = dieFaceOne;
+document.querySelector("img.img2").src = dieFaceTwo;
+
+document.querySelector("body h1").innerText = determineWinner(dieFaceOne, dieFaceTwo);
+
+function determineWinner(dieFaceOne, dieFaceTwo){
+  if(dieFaceOne > dieFaceTwo){
+    return "Player 1 wins!";
+  } else if(dieFaceOne == dieFaceTwo){
+    return "Draw!";
+  } else {
+    return "Player 2 wins!";
+  }
 }
-else if(leftDieFace == rightDieFace)
-{
-  result = "Draw!"
-}
-else
-{
-  result = "Player 2 wins!"
-}
-document.querySelector("h1").textContent = result
