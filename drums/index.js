295002b407c92582d1b188287da16a07ceaacf62
Jason Filippou <jason.filippou@gmail.com>
2022-05-22 17:51:44 -0400

    Improved drums project.

--- drums/index.js
+++ drums/index.js
@@ -1,34 +1,53 @@
 document
   .querySelectorAll(".drum")
-  .forEach((f) => f.addEventListener("click", handleClick(f.innerText)));
+  .forEach(drumElement => drumElement.addEventListener("click", function(){
+        makeSound(this.innerText);
+        animate(this.innerText);
+  }));
 
-function handleClick(keyBinding) {
+document
+  .addEventListener("keydown", function(keyDownEvent) {
+    makeSound(keyDownEvent.key);
+    animate(keyDownEvent.key);
+  });
+
+function makeSound(keyBinding) {
+
   var path = 'sounds/';
   switch(keyBinding){
     case 'w':
       path += 'crash.mp3';
       break;
     case 'a':
-      path += 'kick-bass.mp3'
+      path += 'kick-bass.mp3';
       break;
     case 's':
-      path += 'snare.mp3'
+      path += 'snare.mp3';
       break;
     case 'd':
-      path += 'tom-1.mp3'
+      path += 'tom-1.mp3';
       break;
     case 'j':
-      path += 'tom-2.mp3'
+      path += 'tom-2.mp3';
       break;
     case 'k':
-      path += 'tom-3.mp3'
+      path += 'tom-3.mp3';
       break;
     case 'l':
-      path += 'tom-4.mp3'
+      path += 'tom-4.mp3';
       break;
     default:
-      throw new Error('Key binding ' + keyBinding + " not managed by handler.")
+      break;
   }
   var audio = new Audio(path);
   audio.play();
+}
+
+function animate(drumKey){
+  var buttonElement = document.querySelector('.' + drumKey);
+  buttonElement.classList.add("pressed");
+
+  setTimeout(function(){
+    buttonElement.classList.remove("pressed");
+  }, 100);
 }
