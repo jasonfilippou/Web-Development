a1a3fb707e45ff46ad40a1ba4e62f4d96af1c8b1
Jason Filippou <jason.filippou@gmail.com>
2023-01-15 13:45:23 -0500

    Removed a useless list from the implementation.

--- simon/game.js
+++ simon/game.js
@@ -1,48 +1,42 @@
-/* My global vars */
-var buttonColors = ["red", "blue", "green", "yellow"];
-var gamePattern = [];
-var userClickedPattern = [];
-var level = 0;
-var turnIdx = 0;
+const BUTTON_COLORS = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let level = 0;
+let turnIdx = 0;
 
-/* My functions */
 function nextSequence() {
   $("#level-title").text("Level " + level);
-  var randomChosenColor = buttonColors[randInt(0, buttonColors.length)];
+  let randomChosenColor = BUTTON_COLORS[randInt(0, BUTTON_COLORS.length)];
   gamePattern.push(randomChosenColor);
   flash(randomChosenColor);
   playSound(randomChosenColor);
   level++;
-  turnIdx = 0;      // Resetting the "turn index" for the next evaluation of checkAnswer() in the button handler.
+  turnIdx = 0; // Resetting the "turn index" for the next evaluation of checkAnswer() in the button handler.
 }
 
 /* Handler for key press. Should play the sound corresponding
  * to the randomly selected button above. */
-$(document).keydown(function(){
-   if(gamePattern.length === 0) {
-     nextSequence();
-   }
+$(document).keydown(function () {
+  if (gamePattern.length === 0) {
+    nextSequence();
+  }
 });
 
 /* Handler for clicked button. */
-$(".btn").on("click", function() {
-    if(gamePattern.length === 0) {
+$(".btn").on("click", function () {
+  if (gamePattern.length > 0) {
+    // Don't do anything if we don't have a gamePattern yet
+    let userChosenColor = this.id;
+    if (checkAnswer(userChosenColor)) {
+      animatePress(userChosenColor);
+    } else {
       gameOver();
     }
-    else {
-      var userChosenColor = this.id;
-      if(checkAnswer(userChosenColor)) {
-        animatePress(userChosenColor);
-        userClickedPattern.push(userChosenColor);
-      } else {
-        gameOver();
-      }
-      if(turnIdx === gamePattern.length) {
-        setTimeout( function() {
-            nextSequence(); // Resets "turnIdx"
-        }, 500);     // Wait half a second before you actually call nextSequence();
-      }
+    if (turnIdx === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence(); // Resets "turnIdx"
+      }, 200); // Wait a bit before you actually call nextSequence();
     }
+  }
 });
 
 function checkAnswer(userChosenColor) {
@@ -52,8 +46,8 @@ function checkAnswer(userChosenColor) {
 function gameOver() {
   playSound("wrong");
   $("body").addClass("game-over");
-  setTimeout( function() {
-      $("body").removeClass("game-over");
+  setTimeout(function () {
+    $("body").removeClass("game-over");
   }, 500);
   $("#level-title").text("Game Over, Press Any Key to Restart");
   resetGame();
@@ -61,36 +55,38 @@ function gameOver() {
 
 function resetGame() {
   gamePattern = [];
-  userClickedPattern = [];
   level = 0;
 }
 
 function animatePress(currentColor) {
-    $("#" + currentColor).addClass("pressed");
-    setTimeout( function () {
-      $("#" + currentColor).removeClass("pressed");
-    });
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  });
 }
 
-function playSound(color){
-   var audioFile = new Audio("sounds/" + color + ".mp3");
-   audioFile.play();
+function playSound(color) {
+  let audioFile = new Audio("sounds/" + color + ".mp3");
+  audioFile.play();
 }
 
-function flash(color)  {
-    var FADE_AMT = 100;
-    $("#" + color).fadeOut(FADE_AMT).fadeIn(FADE_AMT).fadeOut(FADE_AMT).fadeIn(FADE_AMT);
+function flash(color) {
+  const FADE_AMT = 100;
+  $("#" + color)
+    .fadeOut(FADE_AMT)
+    .fadeIn(FADE_AMT)
+    .fadeOut(FADE_AMT)
+    .fadeIn(FADE_AMT);
 }
 
-function randInt(low, high){
-  if (!isInt(low) || !isInt(high)){
-    throw "We only accept integer arguments.";
-  }
-  else {
+function randInt(low, high) {
+  if (!isInt(low) || !isInt(high)) {
+    throw new Error("We only accept integer arguments.");
+  } else {
     return Math.floor(Math.random() * (high - low) + low);
   }
 }
 
 function isInt(num) {
-   return $.isNumeric(num) && (Math.floor(num) === num);
+  return $.isNumeric(num) && Math.floor(num) === num;
 }
