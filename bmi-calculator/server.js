33123e8523745d1d87d91801f297254e4e0c27c1
Jason Filippou <jason.filippou@gmail.com>
2023-01-18 07:29:56 -0500

    Working on bmi calculator... again.

--- bmi-calculator/server.js
+++ bmi-calculator/server.js
@@ -1,33 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
 const app = express();
-const path = require('path');
+const port = process.env.port || 3000;
 
-// The following statement allows our `body-parser` instance to access form-data.
-app.use(bodyParser.urlencoded({extended:true}));
-
-// The following statement tells Express to serve all static resources from `public` folder
-// to the client. This is important because when we serve the list.ejs file to the user,
-// their browser also needs to access linked stylesheets and `.js` files.
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(3000, () => {
-  console.log("Server spun on port 3000.")
-});
+app.listen(port, () => console.log(`App listening on port ${port}.`));
 
 app.get("/", (req, res) => {
-  console.log("Serving a GET request at root route.");
-  res.sendFile(path.join(__dirname, "public/list.ejs"));
-});
-
-app.post("/", (req, res) => {
-  console.log("Serving a POST request at root route.");
-  const weight = parseFloat(req.body.weight);
-  const height = parseFloat(req.body.height);
-  let MULTIPLIER = 1;
-  if(req.body.measurements === "imperial") {
-    MULTIPLIER = 703;
-  }
-  const bmi = weight / (height * height) * MULTIPLIER;
-  res.send("<h3>Your BMI is: " + bmi + "</h3>");
+    //res.send('<p>Responding to a GET /</p>');
+    res.sendFile(__dirname + '/views/index.html')
 });
