9fdef0f429cbf463a4156ea5afdacde0161804f4
Jason Filippou <jason.filippou@gmail.com>
2023-01-24 07:42:08 -0500

    Building up towards weather express app.

--- weather/app.js
+++ weather/app.js
@@ -1,4 +1,12 @@
 const { OpenWeatherAPI } = require("openweather-api-node")
+const express = require("express");
+const app = express();
+const PORT = process.env.port || 3000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
+
+app.get("/", (req, res) => {
+    res.send("Serving GET request at root route.");
+});
 
 let weather = new OpenWeatherAPI({
     key: "446796dd3dcb7353a6708022dc53b51a",
