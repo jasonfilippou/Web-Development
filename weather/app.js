87bb2ef7604b1517871f766fc83c0db1fe18f3ea
Jason Filippou <jason.filippou@gmail.com>
2023-01-25 07:47:22 -0500

    Connected stylesheet for error page.

--- weather/app.js
+++ weather/app.js
@@ -1,20 +1,35 @@
-const { OpenWeatherAPI } = require("openweather-api-node")
+const { OpenWeatherAPI } = require("openweather-api-node");
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 const PORT = process.env.port || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
 
 app.get("/", (req, res) => {
-    res.send("Serving GET request at root route.");
+  res.sendFile(path.join(__dirname, "/public/views/index.html"));
 });
 
-let weather = new OpenWeatherAPI({
+app.post("/weather/public", (req, res) => {
+  let city = req.body.city;
+  let units = (req.body.units || "").trim().toUpperCase();
+  if (!["IMPERIAL", "METRIC", "STANDARD"].includes(units)) {
+    res.sendFile(path.join(__dirname, "/public/views/error.html"));
+    return;
+  }
+
+  let weather = new OpenWeatherAPI({
     key: "446796dd3dcb7353a6708022dc53b51a",
-    locationName: "New York",
-    units: "imperial"
+    locationName: city,
+    units: units,
+  });
+  weather.getCurrent().then((data) => {
+    res.send(
+      `<p>Current temperature in ${city} is: ${data.weather.temp.cur}</p>`
+    );
+  });
 });
-
-weather.getCurrent().then(data => {
-    console.log(`Current temperature in New York is: ${data.weather.temp.cur}\u00B0F`)
-})
-
