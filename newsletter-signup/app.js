721edb82c08bad3dcfb81a7b17c47693db6d0146
Jason Filippou <jason.filippou@gmail.com>
2023-01-29 10:05:17 -0500

    Getting an error in the POST request for subscribing a new user.

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+const md5 = require('md5');
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
 const PORT = process.env.port || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,4 +21,21 @@ app.post("/signup", (req, res) => {
   let lastName = req.body.lastName;
   let email = req.body.email;
   console.log(firstName + " " + lastName + " " + email);
+
+  mailchimp.setConfig({
+    apiKey: process.env.API_KEY,
+    server: process.env.SERVER_PREFIX
+  });
+
+  const subscribeIfNotAlreadySubscribed = async () => {
+    const response = await mailchimp.lists.setListMember(
+        process.env.AUDIENCE_ID,
+        md5(email),
+        { email_address: email, status_if_new: "subscribed" }
+    );
+    console.log("Response received from mailchimp: " + response);
+  };
+
+  console.log("Sending conditional subscription request to Mailchimp...")
+  subscribeIfNotAlreadySubscribed().then(r=>r);
 });
